e6479159a06262180e4bf7abc8b5edcd9a409cf5
evanpixelgame <155802426+evanpixelgame@users.noreply.github.com>
2024-04-02 19:47:29 -0400

    Update NewScene.js

--- natquest/js/scenes/NewScene.js
+++ natquest/js/scenes/NewScene.js
@@ -33,11 +33,21 @@ export default class NewScene extends Phaser.Scene {
     const map = this.make.tilemap({ key: 'insidemap' });
     // Load tilesets and create layers
 
-    // Create layers using all tilesets
+    const tilesetsData = [
+        { name: 'tilesheetInterior', key: 'tilesheetInterior' },
+        { name: 'tilesheetWalls', key: 'tilesheetWalls' },
+        { name: 'tilesheetObjects', key: 'tilesheetObjects' },
+    ];
+
     const tilesets = [];
+    tilesetsData.forEach(tilesetData => {
+        tilesets.push(map.addTilesetImage(tilesetData.name, tilesetData.key));
+    });
+
+    // Create layers using all tilesets
     const layers = [];
     for (let i = 0; i < map.layers.length; i++) {
-      layers.push(map.createLayer(i, tilesets, 0, 0));
+        layers.push(map.createLayer(i, tilesets, 0, 0));
     }
 
     // Set world bounds for the player
