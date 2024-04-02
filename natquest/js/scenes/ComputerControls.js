e8344ee2b342cacc779a1708e2d9774aaa6d80c3
evanpixelgame <155802426+evanpixelgame@users.noreply.github.com>
2024-04-02 03:11:50 -0400

    Update ComputerControls.js

--- natquest/js/scenes/ComputerControls.js
+++ natquest/js/scenes/ComputerControls.js
@@ -77,7 +77,7 @@ update(time, delta) {
 
 
         console.log("Received player in PlayerControls:", this.player, this.player.body); // Log player reference  //BOTH LOGGING SUCCESSFULLY
-        console.log("Received velocity in PlayerControls:", this.player.velocity.x, this.player.body.velocity.y);
+        console.log("Received velocity in PlayerControls:", playerBody.velocity.x, playerBody.velocity.y);
 
  
 console.log(this.player);
