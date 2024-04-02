df387ac00841b1f30741ad301318ba9cbaef1570
evanpixelgame <155802426+evanpixelgame@users.noreply.github.com>
2024-04-02 16:00:36 -0400

    Update OpenWorld.js

--- natquest/js/scenes/OpenWorld.js
+++ natquest/js/scenes/OpenWorld.js
@@ -1,26 +1,16 @@
 import { PlayerSprite } from './PlayerSprite.js';
 import { sensorMapSet, createCollisionObjects, sensorHandler } from './collisionHandler.js';
-//import { gameManager } from '../gameState.js';
-//import PlayerControls from './PlayerControls.js';
-
 
 export default class OpenWorld extends Phaser.Scene {
   constructor() {
     super({ key: 'OpenWorld' });
     
-    // Declare controls as a property of the class (should I delete these and put in the init func?
-//   this.controls = null;
     this.map = null;
-    this.player = null;
-  //  this.playerControls = null;
-    this.speed = 2; 
+    this.player = null; 
     this.collisionObjects = null; 
     this.transitionSensors = null; // Add transitionSensors property
     this.engine = null;
    this.world = null;
- //   this.cursors = null;
-   // this.input = null;
-  //  this.velocityChange = null;
   }
 
   init(data) {
@@ -28,39 +18,23 @@ export default class OpenWorld extends Phaser.Scene {
   }
       
   preload() {
-    
   }
        
-  
-  
   create() {
     // Create Matter.js engine
     this.matterEngine = Phaser.Physics.Matter.Matter.World;
     this.engine = this.matter.world;
-//    this.world = this.engine.world;
      this.world = this.matterEngine.create({
     // your Matter.js world options here
   });
-console.log('HELLO THERE PLEASE LOG ' + this.world);
+
     if (this.sys.game.device.os.android || this.sys.game.device.os.iOS) {
         this.scene.launch('MobileControls', { player: this.player, speed: this.speed });
     }
- //  this.controls = null;
- //  this.controls = this.scene.get('ComputerControls'); 
- 
-  
 
-   
- //this.controls = this.scene.launch('ComputerControls', { player: this.player, speed: this.speed });
-  /* this.scene.launch('ComputerControls', { player: this.player, speed: this.speed }, (scene) => {
-  this.controls = scene; // Scene instance passed as argument
-}); */
-    // this.scene.launch('PlayerAnimations', { player: this.player, speed: this.speed });
     this.scene.launch('CompUI', { OpenWorld: this, player: this.player, speed: this.speed, map: this.map, camera: this.cameras.main });
 
-    // Load map
     const map = this.make.tilemap({ key: 'map' });
-
     // Load tileset
     const tilesetsData = [
         { name: 'tilesheetTerrain', key: 'tilesheetTerrain' },
@@ -86,17 +60,12 @@ console.log('HELLO THERE PLEASE LOG ' + this.world);
 
  this.scene.launch('PlayerControls', { player: this.player });
 
- //    this.scene.launch('ComputerControls', { player: this.player, speed: this.speed });
-        //  this.playerControls = new PlayerControls(this.player, this.velocityChange);
-      
     console.log(this.player.body);
     console.log('Player World:', this.player.body.world);
     console.log('Player Body:', this.player.body);
 console.log('Player GameObject:', this.player.gameObject);
           console.log('Player Body GameObject:', this.player.body.gameObject);
              console.log('Player Body GameObject layer:', this.player.body.gameObject.layer);
-
-  //  console.log('Player Layer Index:', this.player.body.gameObject.layer.index);
 
 // Set world bounds for the player
 const boundaryOffset = 2; // Adjust this value as needed
@@ -110,33 +79,18 @@ const worldBounds = new Phaser.Geom.Rectangle(
 this.matter.world.setBounds(0, 0, worldBounds.width, worldBounds.height);
           
            console.log(this.world);
-    // Create collision objects
+
     this.collisionObjects = createCollisionObjects(this, map);
-//    this.transitionSensors = createTransitionSensor(this, map, this.player, this.sensorID); 
-//  console.log('fromopenworldattempt' + transitionSensors[transitionSensor]);
-   this.sensorMapping = sensorMapSet(this, map, this.sensorID);  //this.transitionSensors?
-  // Use TransitionSensorHandler to handle collision events with transition sensors
-this.sensorHandling = sensorHandler(this, map, this.player); //used to have this.transitionSensors as an argument then it became  this.sensorID
+   this.sensorMapping = sensorMapSet(this, map, this.sensorID);  
+this.sensorHandling = sensorHandler(this, map, this.player); 
           
-
-
     // Constrain the camera
     this.cameras.main.setBounds(0, 0, map.widthInPixels, map.heightInPixels);
     this.cameras.main.startFollow(this.player, true, 0.05, 0.05);
     this.cameras.main.setZoom(2);
-
-    
-
   }
-   
-
-
-        
+      
   update(time, delta) {
-    // Update method code here
-  //   if (this.playerControls) {
-    //        this.playerControls.update();
-    //    }
   }
 }
 
