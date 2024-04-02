a86cd47089118292ae370d96a57e2aaa1ffa5a43
evanpixelgame <155802426+evanpixelgame@users.noreply.github.com>
2024-04-02 01:28:38 -0400

    Update InsideRoom.js

--- natquest/js/scenes/InsideRoom.js
+++ natquest/js/scenes/InsideRoom.js
@@ -102,8 +102,8 @@ if (!data || !data.player || !data.speed || !data.camera || !data.controls || !d
     
     this.player.setScale(1); 
 
-    this.scene.add('ComputerControls', ComputerControls); // Add ComputerControls scene
-      this.controls = this.scene.get('ComputerControls'); // Retrieve controls scene
+  //  this.scene.add('ComputerControls', ComputerControls); // Add ComputerControls scene
+//      this.controls = this.scene.get('ComputerControls'); // Retrieve controls scene
 //    this.scene.launch('ComputerControls', { player: this.player, speed: this.speed }); // Launch ComputerControls scene
 //    this.scene.launch('PlayerControls', { player: this.player, speed: 0, velocity: { x: 0, y: 0 } });
     
