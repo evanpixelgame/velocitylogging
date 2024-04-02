99ce8a10eb13a7b36c9c7cd2eee80ae4ad4c8245
evanpixelgame <155802426+evanpixelgame@users.noreply.github.com>
2024-04-02 03:51:32 -0400

    Update PlayerControls.js

--- natquest/js/scenes/PlayerControls.js
+++ natquest/js/scenes/PlayerControls.js
@@ -24,6 +24,12 @@ export default class PlayerControls extends Phaser.Scene {
         this.player = player;
         this.scene = scene;
         this.velocityChange = 2;
+                this.cursors = this.scene.input.keyboard.addKeys({
+            up: Phaser.Input.Keyboard.KeyCodes.W,
+            down: Phaser.Input.Keyboard.KeyCodes.S,
+            left: Phaser.Input.Keyboard.KeyCodes.A,
+            right: Phaser.Input.Keyboard.KeyCodes.D,
+        });
     }
 
     update() {
