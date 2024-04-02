7cd47ec4347952cd4677298ed805fa1191736b28
evanpixelgame <155802426+evanpixelgame@users.noreply.github.com>
2024-04-01 23:03:58 -0400

    Update collisionHandler.js

--- natquest/js/scenes/collisionHandler.js
+++ natquest/js/scenes/collisionHandler.js
@@ -68,7 +68,7 @@ export function sensorHandler(scene, map, player, transitionSensors) {
             
             case 'fastZone':
                 console.log('cue sirens, double speed');
-            scene.speed / 2;
+            scene.speed /= 2;
             break;
 
               case 'BackToOpenWorld':
