f73ee3dd2b95c7985b533b80447152906191ebf7
evanpixelgame <155802426+evanpixelgame@users.noreply.github.com>
2024-04-02 03:26:44 -0400

    Update ComputerControls.js

--- natquest/js/scenes/ComputerControls.js
+++ natquest/js/scenes/ComputerControls.js
@@ -5,17 +5,16 @@ export default class ComputerControls extends Phaser.Scene  {
 
     this.player = null; // Initialize player reference
     this.speed = 0; // Initialize speed
-    this.velocityX = 0; // Pass velocityX
-     this.velocityY = 0; 
+    this.velocityChange = 2;
   }
 
-
+  
     init(data) {
     // Retrieve player reference and speed from the data object
     this.player = data.player;
     this.speed = data.speed;
-     this.velocityX = data.velocityX; // Pass velocityX
-     this.velocityY = data.velocityY; 
+     this.velocityChange = data.velocityChange; // Pass velocityX
+
   console.log("Received player in ComputerControls:", this.player); // Log player reference
   }
 
