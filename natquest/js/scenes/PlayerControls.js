0544d90519acb8495b54ee708ae08e1ca0b682fc
evanpixelgame <155802426+evanpixelgame@users.noreply.github.com>
2024-04-02 00:39:55 -0400

    Update PlayerControls.js

--- natquest/js/scenes/PlayerControls.js
+++ natquest/js/scenes/PlayerControls.js
@@ -5,101 +5,47 @@ export default class PlayerControls extends Phaser.Scene {
         this.player = player;
         this.speed = speed;
         this.velocity = velocity;
- /*
+        this.cursors = null; // Initialize cursors to null
+    }
+
+    create() {
         // Create controls for arrow keys and WASD
-  this.cursors = this.input.keyboard.addKeys({
-    up: Phaser.Input.Keyboard.KeyCodes.W,
-    down: Phaser.Input.Keyboard.KeyCodes.S,
-    left: Phaser.Input.Keyboard.KeyCodes.A,
-    right: Phaser.Input.Keyboard.KeyCodes.D,
-  });
-  */
+        this.cursors = this.input.keyboard.addKeys({
+            up: Phaser.Input.Keyboard.KeyCodes.W,
+            down: Phaser.Input.Keyboard.KeyCodes.S,
+            left: Phaser.Input.Keyboard.KeyCodes.A,
+            right: Phaser.Input.Keyboard.KeyCodes.D,
+        });
+
+        console.log("Received player in PlayerControls:", this.player, this.player.body); // Log player reference
+        console.log("Received velocity in PlayerControls:", this.player.velocity, this.player.body.velocity);
     }
 
-    init(data) {
-    // Retrieve player reference and speed from the data object
-    this.player = data.player;
-   this.velocity = data.velocity;
-  console.log("Received player in PlayerControls:", this.player, this.player.body); // Log player reference
-        console.log("Received velocity in PlayerControls:", this.player.velocity, this.player.body.velocity);
-
-                  // Create controls for arrow keys and WASD
-  this.cursors = this.input.keyboard.addKeys({
-    up: Phaser.Input.Keyboard.KeyCodes.W,
-    down: Phaser.Input.Keyboard.KeyCodes.S,
-    left: Phaser.Input.Keyboard.KeyCodes.A,
-    right: Phaser.Input.Keyboard.KeyCodes.D,
-  });
-  
-  }
-
-  
-  preload() {
-
-  }
-
-  create() {
-
-          // Create controls for arrow keys and WASD
-  this.cursors = this.input.keyboard.addKeys({
-    up: Phaser.Input.Keyboard.KeyCodes.W,
-    down: Phaser.Input.Keyboard.KeyCodes.S,
-    left: Phaser.Input.Keyboard.KeyCodes.A,
-    right: Phaser.Input.Keyboard.KeyCodes.D,
-  });
-  
-
-      
-  }
-
-
-update(time, delta) {
-
-      console.log("Received player in PlayerControls:", this.player, this.player.body); // Log player reference
-        console.log("Received velocity in PlayerControls:", this.player.velocity, this.player.body.velocity);
-    if (!this.player) {
-        return;
-    }
-
-    const playerBody = this.player.body;
-
-const velocityChange = 2; // Adjust based on desired speed
-if (this.cursors.left.isDown) {
-    playerBody.velocity.x = -velocityChange;
-} else if (this.cursors.right.isDown) {
-    playerBody.velocity.x = velocityChange;
-} else {
-    playerBody.velocity.x = 0; // Stop horizontal movement when no input
-}
-
-if (this.cursors.up.isDown) {
-    playerBody.velocity.y = -velocityChange;
-} else if (this.cursors.down.isDown) {
-    playerBody.velocity.y = velocityChange;
-} else {
-    playerBody.velocity.y = 0; // Stop vertical movement when no input
-}
-
-Matter.Body.setVelocity(playerBody, playerBody.velocity);
-
-}
-
-/*
-    // Play appropriate animation based on movement direction
-    if (velocityX !== 0 || velocityY !== 0) {
-        if (velocityX > 0) {
-            this.player.anims.play('walking-right', true);
-        } else if (velocityX < 0) {
-            this.player.anims.play('walking-left', true);
-        } else if (velocityY < 0) {
-            this.player.anims.play('walking-down', true);
-        } else if (velocityY > 0) {
-            this.player.anims.play('walking-up', true);
+    update(time, delta) {
+        // Update player controls logic
+        if (!this.player) {
+            return;
         }
-    } else {
-        // Stop animation when no movement
-        this.player.anims.stop();
+
+        const playerBody = this.player.body;
+
+        const velocityChange = 2; // Adjust based on desired speed
+        if (this.cursors.left.isDown) {
+            playerBody.velocity.x = -velocityChange;
+        } else if (this.cursors.right.isDown) {
+            playerBody.velocity.x = velocityChange;
+        } else {
+            playerBody.velocity.x = 0; // Stop horizontal movement when no input
+        }
+
+        if (this.cursors.up.isDown) {
+            playerBody.velocity.y = -velocityChange;
+        } else if (this.cursors.down.isDown) {
+            playerBody.velocity.y = velocityChange;
+        } else {
+            playerBody.velocity.y = 0; // Stop vertical movement when no input
+        }
+
+        Matter.Body.setVelocity(playerBody, playerBody.velocity);
     }
-   this.player.setRotation(0);
-*/
 }
