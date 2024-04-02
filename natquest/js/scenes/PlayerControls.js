979c85ea6fcb63e958bd2a4e3109a6dfae20a3c7
evanpixelgame <155802426+evanpixelgame@users.noreply.github.com>
2024-04-02 03:35:46 -0400

    Update PlayerControls.js

--- natquest/js/scenes/PlayerControls.js
+++ natquest/js/scenes/PlayerControls.js
@@ -1,3 +1,41 @@
+export default class PlayerControls {
+    constructor(player, scene) {
+        this.player = player;
+        this.scene = scene;
+        this.velocityChange = 2;
+        this.cursors = this.scene.input.keyboard.addKeys({
+            up: Phaser.Input.Keyboard.KeyCodes.W,
+            down: Phaser.Input.Keyboard.KeyCodes.S,
+            left: Phaser.Input.Keyboard.KeyCodes.A,
+            right: Phaser.Input.Keyboard.KeyCodes.D,
+        });
+    }
+
+    update() {
+        const playerBody = this.player.body;
+
+        if (this.cursors.left.isDown) {
+            playerBody.velocity.x = -this.velocityChange;
+        } else if (this.cursors.right.isDown) {
+            playerBody.velocity.x = this.velocityChange;
+        } else {
+            playerBody.velocity.x = 0; // Stop horizontal movement when no input
+        }
+
+        if (this.cursors.up.isDown) {
+            playerBody.velocity.y = -this.velocityChange;
+        } else if (this.cursors.down.isDown) {
+            playerBody.velocity.y = this.velocityChange;
+        } else {
+            playerBody.velocity.y = 0; // Stop vertical movement when no input
+        }
+    }
+}
+
+
+
+
+/*
 export default class PlayerControls extends Phaser.Scene {
     constructor() {
         super({ key: 'PlayerControls' });
@@ -78,3 +116,4 @@ export default class PlayerControls extends Phaser.Scene {
     }
 
 }
+*/
