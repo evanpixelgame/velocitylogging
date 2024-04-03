988dfdd36f0e3f4ccbd3a88c488c622f56792dcd
evanpixelgame <155802426+evanpixelgame@users.noreply.github.com>
2024-04-03 01:53:58 -0400

    Update collisionHandler.js

--- natquest/js/scenes/collisionHandler.js
+++ natquest/js/scenes/collisionHandler.js
@@ -70,8 +70,7 @@ export function sensorHandler(scene, map, player, transitionSensors) {
                 console.log('cue sirens, double speed');
          //   scene.speed /= 2;
             //player.setVelocity(player.velocity.x * 2, player.velocity.y * 2);
-   Matter.Body.setVelocity(this.player.body, { x: this.player.body.velocity.x * 2, y: this.player.body.velocity.y });
-});
+   Matter.Body.setVelocity(scene.player.body, { x: scene.player.body.velocity.x * 2, y: scene.player.body.velocity.y });
             break;
 
               case 'BackToOpenWorld':
