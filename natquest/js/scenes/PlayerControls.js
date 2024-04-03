47013bc2331569fdb1bb62fd3aa2272885d93134
evanpixelgame <155802426+evanpixelgame@users.noreply.github.com>
2024-04-03 01:57:26 -0400

    Update PlayerControls.js

--- natquest/js/scenes/PlayerControls.js
+++ natquest/js/scenes/PlayerControls.js
@@ -33,19 +33,19 @@ export default class PlayerControls extends Phaser.Scene {
 
     if (this.cursors.left.isDown) {
         velocityX = -velocity; // Move left
-        this.player.anims.play('walking-left', true);
+   //     this.player.anims.play('walking-left', true);
     } else if (this.cursors.right.isDown) {
         velocityX = velocity; // Move right
-          this.player.anims.play('walking-right', true);
+   //       this.player.anims.play('walking-right', true);
          
     }
 
     if (this.cursors.up.isDown) {
         velocityY = -velocity; // Move up
-         this.player.anims.play('walking-down', true);
+      //   this.player.anims.play('walking-down', true);
     } else if (this.cursors.down.isDown) {
         velocityY = velocity; // Move down
-        this.player.anims.play('walking-up', true);
+      //  this.player.anims.play('walking-up', true);
     }
 
     // Set the player's velocity directly
@@ -59,9 +59,24 @@ export default class PlayerControls extends Phaser.Scene {
        if (this.player && this.player.body) {
     if (!this.cursors.left.isDown && !this.cursors.right.isDown && !this.cursors.up.isDown && !this.cursors.down.isDown) {
         Matter.Body.setVelocity(playerBody, { x: 0, y: 0 });
-         this.player.anims.stop();
+     //    this.player.anims.stop();
     }
        }
+
+          if (velocityX !== 0 || velocityY !== 0) {
+        if (velocityX > 0) {
+            this.player.anims.play('walking-right', true);
+        } else if (velocityX < 0) {
+            this.player.anims.play('walking-left', true);
+        } else if (velocityY < 0) {
+            this.player.anims.play('walking-down', true);
+        } else if (velocityY > 0) {
+            this.player.anims.play('walking-up', true);
+        }
+    } else {
+        // Stop animation when no movement
+        this.player.anims.stop();
+    }
 }
 
 }
