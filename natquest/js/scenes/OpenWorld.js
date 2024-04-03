9b54c455f184c469581c7354d279114c38522768
evanpixelgame <155802426+evanpixelgame@users.noreply.github.com>
2024-04-03 00:09:45 -0400

    Update OpenWorld.js

--- natquest/js/scenes/OpenWorld.js
+++ natquest/js/scenes/OpenWorld.js
@@ -87,7 +87,7 @@ this.matter.world.setBounds(0, 0, worldBounds.width, worldBounds.height);
     this.cameras.main.setBounds(0, 0, map.widthInPixels, map.heightInPixels);
     this.cameras.main.startFollow(this.player, true, 0.05, 0.05);
     this.cameras.main.setZoom(2);
-
+console.log(this.player);
     // Delay starting the NewScene by 10 seconds
     this.time.delayedCall(2000, () => {
         console.log('Starting NewScene...');
