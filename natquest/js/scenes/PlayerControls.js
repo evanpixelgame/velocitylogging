0fd0e112afcbfb73483ea0a4a1657c49da854b8c
evanpixelgame <155802426+evanpixelgame@users.noreply.github.com>
2024-04-03 01:50:39 -0400

    Update PlayerControls.js

--- natquest/js/scenes/PlayerControls.js
+++ natquest/js/scenes/PlayerControls.js
@@ -33,18 +33,19 @@ export default class PlayerControls extends Phaser.Scene {
 
     if (this.cursors.left.isDown) {
         velocityX = -velocity; // Move left
+        this.player.anims.play('walking-left', true);
     } else if (this.cursors.right.isDown) {
         velocityX = velocity; // Move right
           this.player.anims.play('walking-right', true);
-         this.player.anims.play('walking-left', true);
+         
     }
 
     if (this.cursors.up.isDown) {
         velocityY = -velocity; // Move up
-         this.player.anims.play('walking-up', true);
+         this.player.anims.play('walking-down', true);
     } else if (this.cursors.down.isDown) {
         velocityY = velocity; // Move down
-        this.player.anims.play('walking-down', true);
+        this.player.anims.play('walking-up', true);
     }
 
     // Set the player's velocity directly
