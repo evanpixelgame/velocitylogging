4621cc69abd6d881b59af7fcd268991ec7fed642
evanpixelgame <155802426+evanpixelgame@users.noreply.github.com>
2024-04-09 17:58:08 -0400

    Update OpenWorld.js

--- natquest/js/scenes/OpenWorld.js
+++ natquest/js/scenes/OpenWorld.js
@@ -35,7 +35,7 @@ export default class OpenWorld extends Phaser.Scene {
         this.scene.launch('MobileControls', { player: this.player, speed: this.speed });
     }
 
-    this.scene.launch('CompUI', { OpenWorld: this, player: this.player, speed: this.speed, map: this.map, camera: this.cameras.main });
+    this.scene.launch('CompUI', { OpenWorld: this, player: this.player, map: this.map, camera: this.cameras.main });
         this.scene.launch('PlayerAnimations', { player: this.player, speed: this.speed });
     this.scene.launch('CompUI', { OpenWorld: this, player: this.player, speed: this.speed, map: this.map, camera: this.cameras.main });
 
